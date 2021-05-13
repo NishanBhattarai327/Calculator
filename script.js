505f03672aea9a7d678483fe78533fd53e479663
NishanBhattarai327 <bhattarainishan327@gmail.com>
2021-05-13 15:18:34 +0545

    Update solve function

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ function subtract(x, y) {
 	return x - y;
 }
 function multiply(x, y) {
-	let numX = Number(x);
-	let numY = Number(y);
-
-	if(typeof numX !== 'number' || typeof numY !== 'number') {
-		error('Invalid Input');
-		return;
-	}
-	return numX * numY;
+	return x * y;
 }
 function divide(x, y) {
 	return x / y;
+}
+function modulus(x, y) {
+	return x % y;
 }
 
 function operate(x, operator, y) {
@@ -28,27 +24,24 @@ function operate(x, operator, y) {
 			return multiply(x, y);
 		case '/':
 			return divide(x, y);
+		case '%':
+			return modulus(x, y);
 	}
 }
 
-function updateDisplay(button) {
-	let input = button.innerHTML;
-	display.innerHTML += input;
-}
 
 function backspace() {
 	input.pop();
 	if(input.slice(-1)[0] == ' ') {
 		input.pop();
 	}
-	let output = display.innerHTML.split('');
+	let output = display.textContent.split('');
 	output.pop();
-	display.innerHTML = output.join('');
-	console.log(input);
+	updateDisplay(output.join(''), true);
 }
 
 function clear() {
-	display.innerHTML = '';
+	updateDisplay('', true)
 	input= [];
 	renderMsg('');
 }
@@ -58,44 +51,41 @@ function error(msg) {
 	renderMsg(msg);
 }
 
+function updateDisplay(msg, clear=false) {
+	if(clear) {
+		display.textContent = msg;
+	}
+	else {
+		display.textContent += msg;
+	}
+}
+
 function renderMsg(msg) {
-	message.innerHTML = msg;
+	message.textContent = msg;
 }
 
 function solve() {
 	if(input[0] !== undefined){
 		renderMsg('');
-		let expression = input.join('').split(' ').map(elem => Number(elem) ? Number(elem) : elem);
-		if(expression.length >= 3) {
-			for (let i = 0; i < expression.length; i++) {
-				let iDiv = expression.indexOf('/');
-				let iMul = expression.indexOf('*'); 
-				let iAdd = expression.indexOf('+'); 
-				let iSub = expression.indexOf('-'); 
-				if(iDiv !== -1){
-					let operatingResult = operate(expression[iDiv-1], expression[iDiv], expression[iDiv+1]);
-					expression.splice(iDiv-1, 3, operatingResult);
-				}
-				else if(iMul !== -1){
-					console.log(input);
-					let operatingResult = operate(expression[iMul-1], expression[iMul], expression[iMul+1]);
-					console.log(expression[iMul-1], expression[iMul], expression[iMul+1]);
-					expression.splice(iMul-1, 3, operatingResult);
-				}
-				else if(iAdd !== -1){
-					let operatingResult = operate(expression[iAdd-1], expression[iAdd], expression[iAdd+1]);
-					expression.splice(iAdd-1, 3, operatingResult);
-				}
-				else if(iSub !== -1){
-					let operatingResult = operate(expression[iSub-1], expression[iSub], expression[iSub+1]);
-					expression.splice(iSub-1, 3, operatingResult);
-				}
+		let operators = ['/', '*', '%', '+', '-'];
+		let equation = input.join('').split(' ').map(elem => parseFloat(elem) ? parseFloat(elem) : elem);
+
+		let firstNumber, secondNumber, operator, result, indexOfOperator;
+		for (let i = 0; i < operators.length; i++) {
+			while(equation.includes(operators[i])) {
+				indexOfOperator = equation.indexOf(operators[i]);
+				firstNumber = equation[indexOfOperator-1];
+				operator = equation[indexOfOperator];
+				secondNumber = equation[indexOfOperator+1];
+				result = operate(firstNumber, operator, secondNumber);
+				equation.splice(indexOfOperator-1, 3, result);
 			}
 		}
+
 		clear();
-		renderMsg(expression);
-		input = expression.join('').split('');
-		console.log(expression, input);
+		updateDisplay(equation.join(''), true);
+		input = equation.join('').split('');
+		console.log(equation, input);
 	}
 	else{
 		renderMsg('No Input');
@@ -114,7 +104,7 @@ buttons.forEach((button) => {
 	if (arrayOfButtonClass.indexOf('number') !== -1) {
 		button.addEventListener('click', () => {
 			input.push(event.target.innerHTML);
-			updateDisplay(button);
+			updateDisplay(button.textContent);
 		});
 	}
 	else if (arrayOfButtonClass.indexOf('operator') !== -1) {
@@ -122,7 +112,7 @@ buttons.forEach((button) => {
 			input.push(' ');
 			input.push(event.target.innerHTML);
 			input.push(' ')
-			updateDisplay(button);
+			updateDisplay(button.textContent);
 		});
 	}
 	else if (arrayOfButtonClass.indexOf('equal') !== -1) {
